6425dd31fb084f77b509326bec8e9fe31a825156
Bruno Miguel <brunom.miguel@outlook.com.br>
2023-03-04 20:20:02 -0300

    printing possible moves

--- src/main/java/chess_system/application/UI.java
+++ src/main/java/chess_system/application/UI.java
@@ -51,7 +51,22 @@ public class UI {
 
             for (int j = 0; j < pieces.length; j++) {
                 //imprime a peça
-                printPiece(pieces[i][j]);
+                printPiece(pieces[i][j], false);
+            }
+            //quebra de linha da matriz
+            System.out.println();
+        }
+        System.out.println("  a b c d e f g h");
+    }
+
+    public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+        for (int i = 0; i < pieces.length; i++) {
+            //imprime o numero da linha à esquerda
+            System.out.print( (8 - i) + " ");
+
+            for (int j = 0; j < pieces.length; j++) {
+                //imprime a peça
+                printPiece(pieces[i][j], possibleMoves[i][j]);
             }
             //quebra de linha da matriz
             System.out.println();
@@ -60,9 +75,12 @@ public class UI {
     }
 
     // metodo auxiliar que imprime uma unica peça
-    private static void printPiece(ChessPiece piece) {
+    private static void printPiece(ChessPiece piece, boolean background) {
+        if (background) {
+            System.out.print(ANSI_BLUE_BACKGROUND);
+        }
         if (piece == null) {
-            System.out.print("-");
+            System.out.print("-" + ANSI_RESET);
         }else {
             if (piece.getColor() == Color.WHITE) {
                 System.out.print(ANSI_WHITE + piece + ANSI_RESET);
