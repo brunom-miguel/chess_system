40bc5dfd18083c6e6b2f4be8fcfc8723f60cb3ea
Bruno Miguel <brunom.miguel@outlook.com.br>
2023-03-04 20:40:31 -0300

    implementing possible moves of KING

--- src/main/java/chess_system/chess/pieces/Rook.java
+++ src/main/java/chess_system/chess/pieces/Rook.java
@@ -21,54 +21,54 @@ public class Rook extends ChessPiece {
     public boolean[][] possibleMoves() {
         boolean[][] matriz = new boolean[getBoard().getRows()][getBoard().getColumns()]; // matriz do tamanho do tabuleiro
 
-        Position auxPosition = new Position(0,0); // auxiliar para ter uma posição inicial
+        Position p = new Position(0,0); // auxiliar para ter uma posição inicial
 
         // acima
         // position está definida em "PIECE" (avó de ROOK)
-        auxPosition.setValues(this.position.getRow() -1, this.position.getColumn()); //atribuindo a mesma coluna da peça, porém 1 linha a menos (acima)
-        while (this.getBoard().positionExist(auxPosition) && !getBoard().isThereAPiece(auxPosition)) {
+        p.setValues(this.position.getRow() -1, this.position.getColumn()); //atribuindo a mesma coluna da peça, porém 1 linha a menos (acima)
+        while (this.getBoard().positionExist(p) && !getBoard().isThereAPiece(p)) {
             // enquanto a posição existir e NAO tiver uma peça no lugar, marca como VERDADEIRA
-            matriz[auxPosition.getRow()][auxPosition.getColumn()] = true;
-            auxPosition.setRow(auxPosition.getRow() - 1);
+            matriz[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() - 1);
         }
-        if (this.getBoard().positionExist(auxPosition) && this.isThereOpponentPiece(auxPosition)) {
-            matriz[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        if (this.getBoard().positionExist(p) && this.isThereOpponentPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
         }
 
         // à esquerda
         // position está definida em "PIECE" (avó de ROOK)
-        auxPosition.setValues(this.position.getRow(), this.position.getColumn() -1); //atribuindo a mesma linha da peça, porém 1 coluna a menos (esquerda)
-        while (this.getBoard().positionExist(auxPosition) && !getBoard().isThereAPiece(auxPosition)) {
+        p.setValues(this.position.getRow(), this.position.getColumn() -1); //atribuindo a mesma linha da peça, porém 1 coluna a menos (esquerda)
+        while (this.getBoard().positionExist(p) && !getBoard().isThereAPiece(p)) {
             // enquanto a posição existir e NAO tiver uma peça no lugar, marca como VERDADEIRA
-            matriz[auxPosition.getRow()][auxPosition.getColumn()] = true;
-            auxPosition.setColumn(auxPosition.getColumn() - 1);
+            matriz[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() - 1);
         }
-        if (this.getBoard().positionExist(auxPosition) && this.isThereOpponentPiece(auxPosition)) {
-            matriz[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        if (this.getBoard().positionExist(p) && this.isThereOpponentPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
         }
 
         // à direita
         // position está definida em "PIECE" (avó de ROOK)
-        auxPosition.setValues(this.position.getRow(), this.position.getColumn() +1); //atribuindo a mesma linha da peça, porém 1 coluna a mais (esquerda)
-        while (this.getBoard().positionExist(auxPosition) && !getBoard().isThereAPiece(auxPosition)) {
+        p.setValues(this.position.getRow(), this.position.getColumn() +1); //atribuindo a mesma linha da peça, porém 1 coluna a mais (esquerda)
+        while (this.getBoard().positionExist(p) && !getBoard().isThereAPiece(p)) {
             // enquanto a posição existir e NAO tiver uma peça no lugar, marca como VERDADEIRA
-            matriz[auxPosition.getRow()][auxPosition.getColumn()] = true;
-            auxPosition.setColumn(auxPosition.getColumn() + 1);
+            matriz[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() + 1);
         }
-        if (this.getBoard().positionExist(auxPosition) && this.isThereOpponentPiece(auxPosition)) {
-            matriz[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        if (this.getBoard().positionExist(p) && this.isThereOpponentPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
         }
 
         // abaixo
         // position está definida em "PIECE" (avó de ROOK)
-        auxPosition.setValues(this.position.getRow() +1, this.position.getColumn()); //atribuindo a mesma coluna da peça, porém 1 linha a menos (acima)
-        while (this.getBoard().positionExist(auxPosition) && !getBoard().isThereAPiece(auxPosition)) {
+        p.setValues(this.position.getRow() +1, this.position.getColumn()); //atribuindo a mesma coluna da peça, porém 1 linha a menos (acima)
+        while (this.getBoard().positionExist(p) && !getBoard().isThereAPiece(p)) {
             // enquanto a posição existir e NAO tiver uma peça no lugar, marca como VERDADEIRA
-            matriz[auxPosition.getRow()][auxPosition.getColumn()] = true;
-            auxPosition.setRow(auxPosition.getRow() + 1);
+            matriz[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() + 1);
         }
-        if (this.getBoard().positionExist(auxPosition) && this.isThereOpponentPiece(auxPosition)) {
-            matriz[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        if (this.getBoard().positionExist(p) && this.isThereOpponentPiece(p)) {
+            matriz[p.getRow()][p.getColumn()] = true;
         }
 
         return matriz;
