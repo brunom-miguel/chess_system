b971648be61c2382f6f9df5c9a244208329a35d9
Bruno Miguel <brunom.miguel@outlook.com.br>
2023-03-05 10:50:23 -0300

    Handling captured pieces

--- src/main/java/chess_system/application/UI.java
+++ src/main/java/chess_system/application/UI.java
@@ -5,8 +5,11 @@ import chess_system.chess.ChessPiece;
 import chess_system.chess.ChessPosition;
 import chess_system.chess.Color;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class UI {
 
@@ -45,8 +48,10 @@ public class UI {
         }
     }
 
-    public static void printMatch(ChessMatch chessMatch) {
+    public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
         printBoard(chessMatch.getPieces());
+        System.out.println();
+        printCapturedPieces(captured);
         System.out.println();
         System.out.println("Turn: " + chessMatch.getTurn());
         System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
@@ -97,5 +102,21 @@ public class UI {
             }
         }
         System.out.print(" ");
+    }
+
+    //responsável por imprimir na tela a lista de peças capturadas
+    private static void printCapturedPieces(List<ChessPiece> captured) {
+        List<ChessPiece> white = captured.stream().filter(item -> item.getColor() == Color.WHITE).collect(Collectors.toList());
+        List<ChessPiece> black = captured.stream().filter(item -> item.getColor() == Color.BLACK).collect(Collectors.toList());
+        System.out.println("Captured pieces:");
+        System.out.print("White: ");
+        System.out.print(ANSI_WHITE);
+        System.out.println(Arrays.toString(white.toArray()));
+        System.out.print(ANSI_RESET);
+        System.out.print("Black: ");
+        System.out.print(ANSI_YELLOW);
+        System.out.println(Arrays.toString(black.toArray()));
+        System.out.print(ANSI_RESET);
+
     }
 }
